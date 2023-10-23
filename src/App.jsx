689e7eecf560be44c073ac58e63d776873f734ab
Matheus Nascimento <matheuspeluso@gmail.com>
2023-10-23 20:49:09 -0300

    formulario multistep com react

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+// Components
+import {GrFormNext, GrFormPrevious} from "react-icons/gr";
+import {FiSend} from "react-icons/fi"
+
+import './App.css'
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+
+//Hooks
+import {useForm} from "./hooks/useForm"
+
+function App() {
+
+  const formComponents = [<UserForm/>,<ReviewForm/>,<Thanks/>];
+  // apos a criaçao do array vamos iniciar o hook
+
+  const {currentStep,currentComponent,changeStep,isLastStep,isFirstStep} = useForm(formComponents)
+  console.log(isLastStep);
+  return (
+    <div className='app'>
+        <div className="header">
+          <h2>Deixe sua avaliação</h2>
+          <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto.</p>
+        </div>
+
+        <div className="form-container">
+          <p>Etapas</p>
+          <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+            <div className="inputs-container">{currentComponent}</div>
+            <div className="actions">
+              {!isFirstStep && (<button type='button' onClick={()=> changeStep(currentStep - 1)}>
+                <GrFormPrevious/>
+                <span>Voltar</span>
+              </button>)}
+              {!isLastStep ? (
+                <button type='submit'>
+                <span>Avançar</span>
+                <GrFormNext/>
+              </button>
+              ) : (<button type='button'>
+              <span>Enviar</span>
+              <FiSend/>
+            </button>) }
+            </div>
+          </form>
+
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export function useForm(steps){
+    //tenho que retornar o passo que estamos
+    const [currentStep, setCurrentStep] = useState(0)
+
+    function changeStep(i,e){
+        if(e) e.preventDefault();
+
+        
+
+        if(i <0 || i >= steps.length) return
+
+        setCurrentStep(i)
+    }
+    return{
+        currentStep,
+        currentComponent: steps[currentStep],
+        changeStep,
+        isLastStep: currentStep + 1 === steps.length ? true : false,
+        isFirstStep: currentStep === 0 ? true : false,
+    }
+}
