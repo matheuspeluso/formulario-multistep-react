8fe61fdfeda0cbcba5af9b20f4e3817f7be648cc
Matheus Nascimento <matheuspeluso@gmail.com>
2023-10-24 20:18:01 -0300

    rafctor: added initial global styles

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -3,7 +3,27 @@ import React from 'react'
 const UserForm = () => {
   return (
     <div>
-        <h2> User Form!</h2>
+        <div className="form-control">
+          <label htmlFor='name'>Nome:</label>
+          <input 
+          type="text" 
+          name='text' 
+          id='name' 
+          placeholder='Digite seu nome' 
+          required
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor='name'>E-mail:</label>
+          <input 
+          type="email" 
+          name='email' 
+          id='email' 
+          placeholder='Digite seu e-mail' 
+          required
+          />
+        </div>
     </div>
   )
 }
