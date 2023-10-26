50a96af727d2453ef65bfdece5314c4b47790adf
Matheus Nascimento <matheuspeluso17@gmail.com>
2023-10-26 15:33:33 -0300

    rafactor: added the steps componnets

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import UserForm from "./components/UserForm";
 import ReviewForm from "./components/ReviewForm";
 import Thanks from "./components/Thanks";
+import Steps from "./components/steps";
 
 //Hooks
 import {useForm} from "./hooks/useForm"
@@ -25,7 +26,7 @@ function App() {
         </div>
 
         <div className="form-container">
-          <p>Etapas</p>
+          <Steps currentStep= {currentStep}/>
           <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
             <div className="inputs-container">{currentComponent}</div>
             <div className="actions">
